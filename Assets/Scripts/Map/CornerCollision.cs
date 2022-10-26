d9da9bc1fb635df6bf20eb539fd8ac578e543a68
Chayan Tronson <chayan995@gmail.com>
2022-10-25 18:19:01 -0700

    simplified wall and corner overlap checking

--- Assets/Scripts/Map/CornerCollision.cs
+++ Assets/Scripts/Map/CornerCollision.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CornerCollision : MonoBehaviour
-{
+public class CornerCollision : MonoBehaviour {
+    /**
+     * Destroys overlapping corners.
+     */
     void Start() {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.01f);
         foreach (Collider2D c in colliders) {
-            if (c.CompareTag("Corner") && (c.gameObject != gameObject)) {
+            if (c.CompareTag("Corners") && (c.gameObject != gameObject)) {
                 Destroy(c.gameObject);
             }
         }
--- Assets/Scripts/Map/WallCollision.cs
+++ Assets/Scripts/Map/WallCollision.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 
 public class WallCollision : MonoBehaviour {
     /**
-     * The group with this script does the collision check.
-     * The other group with the tag gets removed.
-     *
-     * For example:
-     * North walls (have this script), and they destroy overlapping South walls (have tag)
-     * West walls (have this script), and they destroy East walls (have tag)
+     * Destroys other walls that overlap with this one (if they have the tag).
      *
      * Note: Standard collision entry functions don't work.
      */
     void Start() {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.01f);
         foreach (Collider2D c in colliders) {
-            if (c.CompareTag("WallRemovable")) {
+            if (c.CompareTag("Walls") && (c.gameObject != gameObject)) {
                 Destroy(c.gameObject);
-                return; // only need to destroy 1. Ex: North does South, West does East.
             }
         }
     }
