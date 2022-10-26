424285ae84db36e9f98fbb0de1037bed14fb1559
Chayan Tronson <chayan995@gmail.com>
2022-10-25 17:55:49 -0700

    optimized walls
    destroying overlapping walls

--- Assets/Scripts/Map/WallCollision.cs
+++ Assets/Scripts/Map/WallCollision.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WallCollision : MonoBehaviour {
-    void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("RoomWall")) {
-            Destroy(gameObject);
+    /**
+     * The group with this script does the collision check.
+     * The other group with the tag gets removed.
+     *
+     * For example:
+     * North walls (have this script), and they destroy overlapping South walls (have tag)
+     * West walls (have this script), and they destroy East walls (have tag)
+     *
+     * Note: Standard collision entry functions don't work.
+     */
+    void Start() {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.01f);
+        foreach (Collider2D c in colliders) {
+            if (c.CompareTag("WallRemovable")) {
+                Destroy(c.gameObject);
+                return; // only need to destroy 1. Ex: North does South, West does East.
+            }
         }
     }
 }
