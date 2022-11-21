30a62f8f33b806948969cb9a0ec847f7c11afd4a
bry-gavino <bry_gavino01@berkeley.edu>
2022-11-20 18:50:48 -0800

    added new enemy

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -336,7 +336,6 @@ public class PlayerController : MonoBehaviour {
                 }
                 if (chargingAttack) {
                     PlayerRB.velocity = PlayerRB.velocity/3;
-                    Debug.Log(PlayerRB.velocity);
                 }
                 Effects.HandleDirection(currDirection);
             }
@@ -380,7 +379,6 @@ public class PlayerController : MonoBehaviour {
                 musicManager.playClip(Charge1FX, 1);
             }
             chargingAttack = true;
-            Debug.Log("CHARGING");
         } else {
             musicManager.playClip(AttackFX, 1);
             if (combo == 3) {
@@ -388,7 +386,6 @@ public class PlayerController : MonoBehaviour {
             } else {
                 combo += 1;
             }
-            Debug.Log("combo: "+combo);
             dashLengthTimer = attackLunge; // propels character forward (like a lunge)
             dashCooldownTimer = attackLength;
             attackTimer = attackLength;
@@ -402,7 +399,6 @@ public class PlayerController : MonoBehaviour {
         musicManager.playClip(ReleaseFX, 1);
         chargingAttack = false;
         releasingChargeAttack = true;
-        Debug.Log("RELEASE");
         attackTimer = chargeAttackLength;
         attackCooldownTimer = chargeAttackCooldown;
         isAttacking = true;
@@ -419,7 +415,6 @@ public class PlayerController : MonoBehaviour {
     public void OnAttackTriggerEnter2D(Collider2D col) {
         if(isAttacking) {
             float damageCombo = Damage + Damage*((combo-1)/3.0f);
-            Debug.Log("damageCombo: "+damageCombo);
             if (releasingChargeAttack) {
                 damageCombo = Damage*2;
             }
@@ -447,7 +442,6 @@ public class PlayerController : MonoBehaviour {
             anim.SetBool("Hurt", true);
             hurtCooldownTimer = hurtCooldown;
             currHealth -= val;
-            Debug.Log("health is now " + currHealth.ToString());
         }
         if (currHealth <= 0) {
             Die();
@@ -489,7 +483,7 @@ public class PlayerController : MonoBehaviour {
     public void Heal(float val) {
         if (inventory.Count > 0) {
             currHealth = Mathf.Min(currHealth + val, maxHealth);
-            Debug.Log("health is now " + currHealth.ToString());
+            // Debug.Log("health is now " + currHealth.ToString());
 
             HPSlider.value = currHealth / maxHealth;
 
@@ -517,12 +511,12 @@ public class PlayerController : MonoBehaviour {
             Damage = Damage + (0.01f + ((100 - currLevel) * 0.001f));
             // Debug.Log("New Damage: "+Damage);
             maxHealth = maxHealth + (0.01f + ((100 - currLevel) * 0.001f));
-            Debug.Log("New maxHealth: "+maxHealth);
+            // Debug.Log("New maxHealth: "+maxHealth);
             currHealth = currHealth + (maxHealth/3.0f);
             if (currHealth > maxHealth) {
                 currHealth = maxHealth;
             }
-            Debug.Log("New currHealth: "+currHealth);
+            // Debug.Log("New currHealth: "+currHealth);
             HPSlider.value = currHealth / maxHealth;
             LevelTxt.text = currLevel.ToString();
         }
